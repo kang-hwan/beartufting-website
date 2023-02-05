fde1c7b020ef1ebf9b3bf180357ff701fb31c748
Roy Kanghwan Kim <roykim0416@gmail.com>
2023-02-06 01:38:57 +1100

    Form Section Update v3

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack, Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { v4 as uuidv4 } from "uuid";
 import "./BookingForm.css";
+import { width } from "@mui/system";
 
 export default function BookingForm() {
   // ! POST request
@@ -19,16 +20,17 @@ export default function BookingForm() {
       },
       body: JSON.stringify({
         id: uuidv4(),
-        name: "",
-        mobile: "",
+        submitTime: new Date(),
+        name: "test",
+        mobile: "test",
         email: "",
-        tufting: "",
-        punchNeedle: "",
-        fluidArt: "",
-        illustration: "",
-        kidsVoucher: "",
-        totalAttend: "",
-        totalDeposit: "",
+        tufting: tfCount,
+        punchNeedle: pnCount,
+        fluidArt: faCount,
+        illustration: ilCount,
+        kidsVoucher: kvCount,
+        totalAttend: totalAttend,
+        totalDeposit: totalDeposit,
         message: "",
       }),
     });
@@ -37,6 +39,75 @@ export default function BookingForm() {
     console.log("data: ", data);
   };
 
+  // ! Counters
+  // Tufting Class
+  const [tfCount, setTfCount] = useState(0);
+  function decrementTf() {
+    if (tfCount > 0) {
+      setTfCount(tfCount - 1);
+    }
+  }
+
+  function incrementTf() {
+    setTfCount(tfCount + 1);
+  }
+
+  // Punch Needle Class
+  const [pnCount, setPnCount] = useState(0);
+  function decrementPn() {
+    if (pnCount > 0) {
+      setPnCount(pnCount - 1);
+    }
+  }
+
+  function incrementPn() {
+    setPnCount(pnCount + 1);
+  }
+
+  // Fluid Art
+  const [faCount, setFaCount] = useState(0);
+  function decrementFa() {
+    if (faCount > 0) {
+      setFaCount(faCount - 1);
+    }
+  }
+
+  function incrementFa() {
+    setFaCount(faCount + 1);
+  }
+
+  // Image Illustration
+  const [ilCount, setIlCount] = useState(0);
+  function decrementIl() {
+    if (ilCount > 0) {
+      setIlCount(ilCount - 1);
+    }
+  }
+
+  function incrementIl() {
+    setIlCount(ilCount + 1);
+  }
+
+  // Creative Kids Voucher
+  const [kvCount, setKvCount] = useState(0);
+  function decrementKv() {
+    if (kvCount > 0) {
+      setKvCount(kvCount - 1);
+    }
+  }
+
+  function incrementKv() {
+    setKvCount(kvCount + 1);
+  }
+
+  // Total Attend
+  const totalAttend = tfCount + pnCount + faCount;
+
+  // Deposits Total
+  const totalDeposit =
+    (tfCount + pnCount + faCount + ilCount) * 50 + kvCount * -100;
+
+  // ! Form Return
   return (
     <form className="form-container" onSubmit={(e) => submitHandler(e)}>
       <h3 style={{ fontWeight: "bold", paddingBottom: "15px" }}>
@@ -86,41 +157,53 @@ export default function BookingForm() {
           <Stack direction="row" spacing={3} className="class-stack">
             <h5 className="class-title">Tufting</h5>
             <div style={{ display: "flex" }}>
-              <button className="btn-decrement">-</button>
+              <button className="btn-decrement" onClick={decrementTf}>
+                -
+              </button>
               <h5
                 style={{ backgroundColor: "whitesmoke", padding: "0 10px 0" }}
               >
-                0
+                {tfCount}
               </h5>
-              <button className="btn-increment">+</button>
+              <button className="btn-increment" onClick={incrementTf}>
+                +
+              </button>
             </div>
-            <h5>$50</h5>
+            <h5 style={{ width: "50px" }}>${tfCount * 50}</h5>
           </Stack>
           <Stack direction="row" spacing={3} className="class-stack">
             <h5 className="class-title">Punch Needle</h5>
             <div style={{ display: "flex" }}>
-              <button className="btn-decrement">-</button>
+              <button className="btn-decrement" onClick={decrementPn}>
+                -
+              </button>
               <h5
                 style={{ backgroundColor: "whitesmoke", padding: "0 10px 0" }}
               >
-                0
+                {pnCount}
               </h5>
-              <button className="btn-increment">+</button>
+              <button className="btn-increment" onClick={incrementPn}>
+                +
+              </button>
             </div>
-            <h5>$50</h5>
+            <h5 style={{ width: "50px" }}>${pnCount * 50}</h5>
           </Stack>
           <Stack direction="row" spacing={3} className="class-stack">
             <h5 className="class-title">Fluid Art</h5>
             <div style={{ display: "flex" }}>
-              <button className="btn-decrement">-</button>
+              <button className="btn-decrement" onClick={decrementFa}>
+                -
+              </button>
               <h5
                 style={{ backgroundColor: "whitesmoke", padding: "0 10px 0" }}
               >
-                0
+                {faCount}
               </h5>
-              <button className="btn-increment">+</button>
+              <button className="btn-increment" onClick={incrementFa}>
+                +
+              </button>
             </div>
-            <h5>$50</h5>
+            <h5 style={{ width: "50px" }}>${faCount * 50}</h5>
           </Stack>
           <p
             style={{
@@ -135,29 +218,38 @@ export default function BookingForm() {
           <Stack direction="row" spacing={3} className="addOn-stack">
             <h5 className="class-title">Image Illustration</h5>
             <div style={{ display: "flex" }}>
-              <button className="btn-decrement">-</button>
+              <button className="btn-decrement" onClick={decrementIl}>
+                -
+              </button>
               <h5
                 style={{ backgroundColor: "whitesmoke", padding: "0 10px 0" }}
               >
-                0
+                {ilCount}
               </h5>
-              <button className="btn-increment">+</button>
+              <button className="btn-increment" onClick={incrementIl}>
+                +
+              </button>
             </div>
-            <h5>$50</h5>
+            <h5 style={{ width: "50px" }}>${ilCount * 50}</h5>
           </Stack>
           <Stack direction="row" spacing={3} className="addOn-stack">
             <h5 className="class-title">Creative Kids Voucher</h5>
             <div style={{ display: "flex" }}>
-              <button className="btn-decrement">-</button>
+              <button className="btn-decrement" onClick={decrementKv}>
+                -
+              </button>
               <h5
                 style={{ backgroundColor: "whitesmoke", padding: "0 10px 0" }}
               >
-                0
+                {kvCount}
               </h5>
-              <button className="btn-increment">+</button>
+              <button className="btn-increment" onClick={incrementKv}>
+                +
+              </button>
             </div>
-            <h5>$50</h5>
+            <h5 style={{ width: "50px" }}>${kvCount * -100}</h5>
           </Stack>
+          <Stack>Deposits Total: ${totalDeposit}</Stack>
         </Box>
         <Box>
           <TextField
